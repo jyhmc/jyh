38dad6b8b49509b75a4ed3ef05cf13595a4f5867
jyhmc <jyhmc@sina.com>
2017-04-01 17:19:39 +0800

    .

---
+++ WebContent/views/yzzcapp/app/system/app/store/MenuTreeByRoleStore.js
@@ -0,0 +1,30 @@
+Ext.define('System.store.MenuTreeByRoleStore', {
+    extend: 'Ext.data.TreeStore',
+    alias:'store.MenuTreeByRoleStore',
+    requires: [
+        'System.model.MenuTreeModel',
+        'Ext.data.proxy.Ajax',
+        'Ext.data.reader.Json'
+    ],
+    constructor: function(cfg) {
+        var me = this;
+        cfg = cfg || {};
+        me.callParent([Ext.apply({
+        	//autoLoad:true,
+            model: 'System.model.MenuTreeModel',
+            defaultRootId :0,
+            root : {
+				text : '功能权限',
+				expanded:true
+			},
+            proxy: {
+                type:'ajax',
+                url :__ctxPath + '/sys/selectMenuByRole',
+                reader:{
+                	type:'json',
+                	root:'result'
+                }
+            }
+        }, cfg)]);
+    }
+});
