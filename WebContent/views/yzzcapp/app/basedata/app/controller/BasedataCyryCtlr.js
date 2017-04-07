4ca881c78e92c38454173d4b1d293c1b46fbf845
jyhmc <jyhmc@sina.com>
2017-04-07 16:45:37 +0800

    base

---
+++ WebContent/views/yzzcapp/app/basedata/app/controller/BasedataCyryCtlr.js
@@ -0,0 +1,29 @@
+Ext.define('Basedata.controller.BasedataCyryCtlr', {
+	extend : 'Ext.app.Controller',
+	alias : 'controller.BasedataCyryCtlr',
+	views : [ 'BasedataCyryView' ],
+	refs : [ {
+		ref : 'DeptGridpanel',
+		selector : 'SystemDeptView gridpanel[itemId=DeptGridpanel]'
+	} ],
+
+	onDeptTreepanelItemClick : function(is, record, item, index, e, eOpts) {
+		var me = this;
+		var store = me.getDeptGridpanel().getStore();
+		var node = record.data.id;
+		store.on("beforeload", function() {
+			Ext.apply(store.proxy.extraParams, {
+				'node' : node
+			});
+		});
+		store.loadPage(1);
+	},
+
+	init : function(application) {
+		this.control({
+			"SystemDeptView  treepanel[itemId=DeptTreepanel]" : {
+				itemclick : this.onDeptTreepanelItemClick
+			}
+		});
+	}
+});
