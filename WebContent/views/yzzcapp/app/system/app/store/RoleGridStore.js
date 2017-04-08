c0f13c0d5a7ff77d3c48be6842a7a4bb7d093ed1
jyhmc <jyhmc@sina.com>
2017-04-08 14:23:27 +0800

    zheteng

--- WebContent/views/yzzcapp/app/system/app/store/RoleGridStore.js
+++ WebContent/views/yzzcapp/app/system/app/store/RoleGridStore.js
@@ -1,26 +1,22 @@
 Ext.define('System.store.RoleGridStore', {
-    extend: 'Ext.data.Store',
-    alias:'store.RoleGridStore',
-    requires: [
-        'System.model.RoleGridModel',
-        'Ext.data.proxy.Ajax',
-        'Ext.data.reader.Json'
-    ],
-    constructor: function(cfg) {
-        var me = this;
-        cfg = cfg || {};
-        me.callParent([Ext.apply({
-            model: 'System.model.RoleGridModel',
-            autoLoad:true,
-            proxy: {
-                type:'jsonp',
-                //callbackKey: "jsonPCallback",
-                url : 'http://192.168.1.106:8080/yzzc/sys/selectRole',
-                reader:{
-                	type:'json',
-                	root:'result'
-                }
-            }
-        }, cfg)]);
-    }
+	extend : 'Ext.data.Store',
+	alias : 'store.RoleGridStore',
+	requires : [ 'System.model.RoleGridModel', 'Ext.data.proxy.Ajax',
+			'Ext.data.reader.Json' ],
+	constructor : function(cfg) {
+		var me = this;
+		cfg = cfg || {};
+		me.callParent([ Ext.apply({
+			model : 'System.model.RoleGridModel',
+			autoLoad : true,
+			proxy : {
+				type : 'ajax',
+				url : __ctxPath + '/sys/selectRole',
+				reader : {
+					type : 'json',
+					root : 'result'
+				}
+			}
+		}, cfg) ]);
+	}
 });
